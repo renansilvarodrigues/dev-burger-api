7ebc4c6bd592a4a160727a1744f3f1c972c47fa8
renansilvarodrigues <renansilva6769@gmail.com>
2026-01-18 23:20:08 -0300

    commit 1801_2

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-import app from "./app.js";
+import app from './app.js';
 
 app.listen(3001, () => {
-  console.log("Server is running on port 3001");
+  console.log('Server is running on port 3001');
 });
