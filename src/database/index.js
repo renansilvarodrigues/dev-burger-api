857608b41518ef4a773cae91f8eaedb15d50aca5
renansilvarodrigues <renansilva6769@gmail.com>
2026-06-20 15:26:36 -0300

    commit 20062026

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import { Sequelize } from 'sequelize';
+import mongoose from 'mongoose';
 import databaseConfig from '../config/database.cjs';
 import User from '../app/models/User.js';
 import Product from '../app/models/Product.js';
@@ -9,6 +10,7 @@ const models = [User, Product, Category];
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
   init() {
     this.connection = new Sequelize(databaseConfig);
@@ -17,6 +19,12 @@ class Database {
       .map(
         (model) => model.associate && model.associate(this.connection.models),
       );
+  }
+
+  mongo() {
+    this.mongooseConnection = mongoose.connect(
+      'mongodb://localhost:27017/devburger',
+    );
   }
 }
 
